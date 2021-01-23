36d6a41a01f141bb58d1fdb152d9c461d3e46da2
Raja's <gkrajareddy@gmail.com>
2021-01-23 14:38:43 +0530

    starting

---
+++ packages/calci.py
@@ -0,0 +1,14 @@
+def add(n1, n2=0):
+    return n1 + n2
+
+def sub(n1, n2=0):
+    return n1 - n2
+def mul(n1, n2=1):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+def fdiv(n1, n2):
+    return n1//n2
+def exp(n1, n2):
+    return n1**n2
---
+++ packages/myMath.py
@@ -0,0 +1,9 @@
+def isEven(num):
+    if num%2==0:
+        return True
+    return False
+
+def isOdd(num):
+    if num%2==0:
+        return False
+    return True
